f6b8e7a3360c32845c9413bcad2e981bc58826ff
PibouleauJB <jean-baptiste.pibouleau@etu.univ-amu.fr>
2025-01-06 19:30:01 +0100

    Mode Sombre + Flèche Haut
    Mode sombre JS + CSS
    Flèche haut JS

--- main.js
+++ main.js
@@ -2,8 +2,22 @@ function darkFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
-function cRetour() {
-  window.onscroll = function(ev) {
-    document.getElementById("cRetour").className = (window.pageYOffset > 100) ? "cVisible" : "cInvisible";
-  };
-}
+// Sélection du bouton
+const HautButton = document.getElementById("Haut");
+
+// Ajouter un écouteur sur le défilement de la fenêtre
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        HautButton.style.display = "block";
+    } else {
+        HautButton.style.display = "none";
+    }
+});
+
+// Ajouter un écouteur sur le clic du bouton
+HautButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Défilement fluide
+    });
+});
